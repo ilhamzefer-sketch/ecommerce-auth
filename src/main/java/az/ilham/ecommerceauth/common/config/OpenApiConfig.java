7da5c4e0625d6ff7abeea306ad3d2edf71aa1709
ilham safarov <ilhamzefer@gmail.com>
2026-06-06 21:54:45 +0400

    Route Swagger requests through API gateway

--- src/main/java/az/ilham/ecommerceauth/common/config/OpenApiConfig.java
+++ src/main/java/az/ilham/ecommerceauth/common/config/OpenApiConfig.java
@@ -4,16 +4,25 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    public OpenAPI customOpenAPI(
+            @Value("${application.openapi.gateway-url}") String gatewayUrl
+    ) {
         final String securitySchemeName = "bearerAuth";
         return new OpenAPI()
+                .servers(List.of(new Server()
+                        .url(gatewayUrl)
+                        .description("API Gateway")))
                 .info(new Info()
                         .title("E-commerce Auth API")
                         .version("1.0")
